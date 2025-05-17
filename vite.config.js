5b553a840fdd5d3ef9cc82569abeed5e2cf37da8
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2025-05-17 15:57:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
     server: {
     host: '0.0.0.0',
     port: 7865,
-  }
+  },
+   preview: {
+    host: '0.0.0.0',
+    port: 7865,
+    allowedHosts: ['milkdashboard.onrender.com'], 
+  },
 })
