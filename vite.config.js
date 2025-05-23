55ce6da89ec50594a31a783d730ae48436864ae3
thabumon <52034689+thabumon@users.noreply.github.com>
2025-05-23 19:38:08 +0530

    EDTZ

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
     host: '0.0.0.0',
-    port: 4173,
+    port: parseInt(process.env.PORT) || 4173,
   },
   preview: {
-  host: '0.0.0.0',
-  port: 4173,
-  allowedHosts: ['milkdashboard.onrender.com', 'admin.palkkaran.in'],
-},
-
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['milkdashboard.onrender.com', 'admin.palkkaran.in'],
+  },
 })
