88174d075daa87160bbbfc602fd071f7add402f8
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-07-13 17:29:27 +0530

    feat: config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+server: {
+  host: '0.0.0.0',
+  port: parseInt(process.env.PORT) || 4173, // ✅ Uses the PORT from Render if available
+},
+preview: {
+  host: '0.0.0.0',
+  port: parseInt(process.env.PORT) || 4173, // ✅ Same here for preview mode
+  allowedHosts: ['milkdashboard.onrender.com', 'admin.palkkaran.in'],
+},
+
 })
