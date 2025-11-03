7171d6e5b301a0b6f36bb7afdc74b893a52c592d
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-11-03 08:19:27 +0530

    fix: update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ✅ Add this line to make asset paths relative (fixes CSS/JS loading issue)
-
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173, // ✅ Uses PORT if provided
