35f925a6cacd3f62ab9b0b9fa4cc2367e838db2b
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-11-08 08:47:26 +0530

    feat: new update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173, // ✅ Uses PORT if provided
+    port: parseInt(process.env.PORT) || 4173,
   },
-
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
